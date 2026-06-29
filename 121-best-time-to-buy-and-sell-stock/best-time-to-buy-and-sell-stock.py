513f98a3a24d0a356a97f8014f5e3b4d2308949b
chanbin park <chanbinp3@gmail.com>
2026-06-28 20:55:50 -0500

    Time: 37 ms (76.83%) | Memory: 28.6 MB (40.89%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # you want to find the lowest buy and the highest price after that
+        # two pointer where you update buy (when you come across a lower
+        # buy) and then the valure youre currently looking at to see if you
+        # surpass the global max diff
+
+        l = 0
+        maxprof = 0
+
+        for r in range(len(prices)):
+            if prices[r] < prices[l]:
+                l = r
+            elif prices[r] - prices[l] > maxprof:
+                maxprof = prices[r] - prices[l]
+        return maxprof
+
+        
